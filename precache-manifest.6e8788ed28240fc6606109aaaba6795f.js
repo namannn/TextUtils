cfa9e58a0c5f0bbeb2ecd5a3b645c163b4b6e004
namannn <46934223+namannn@users.noreply.github.com>
2022-02-03 19:40:31 +0530

    Updates

---
+++ precache-manifest.6e8788ed28240fc6606109aaaba6795f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3602c7594a8747ed7aedbc4782c0ff7b",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "a06f8153ab2a3ce939d3",
+    "url": "/TextUtils/static/css/main.532dc913.chunk.css"
+  },
+  {
+    "revision": "bf7b78d780164643841f",
+    "url": "/TextUtils/static/js/2.f4de9046.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/TextUtils/static/js/2.f4de9046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d76ac6b552a0ac4e51",
+    "url": "/TextUtils/static/js/3.7affafde.chunk.js"
+  },
+  {
+    "revision": "a06f8153ab2a3ce939d3",
+    "url": "/TextUtils/static/js/main.d064a1bb.chunk.js"
+  },
+  {
+    "revision": "e2b30b7ad5d9d9eb5ca8",
+    "url": "/TextUtils/static/js/runtime-main.82c2e0a1.js"
+  }
+]);
